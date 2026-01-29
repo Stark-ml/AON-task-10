797834798f10ab3c4b534664cefb49c56da1f690
Stark-ml <zeref.sama.gilgamesh@gmail.com>
2026-01-29 12:33:04 +0300

    final 2

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -2,7 +2,7 @@ export default function MealCard({ meal, onClick }) {
   return (
     <div
       onClick={() => onClick(meal)}
-      className="bg-white text-black rounded-xl shadow cursor-pointer
+      className="bg-slate-800 text-black rounded-xl shadow cursor-pointer
                  hover:scale-105 hover:shadow-lg transition"
     >
       <img
@@ -11,7 +11,7 @@ export default function MealCard({ meal, onClick }) {
         className="rounded-t-xl"
       />
       <div className="p-4">
-        <h2 className="font-semibold text-lg">
+        <h2 className="font-semibold text-white">
           {meal.strMeal}
         </h2>
       </div>
